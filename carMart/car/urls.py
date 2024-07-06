1c9a9e4dc15ddb5e83f5ee6a17c1280e54475cf2
Fazle Rabbi <142036307+Fazle-Rabbi72@users.noreply.github.com>
2024-07-06 14:12:09 +0600

    Delete carMart directory

--- carMart/car/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path,include
-from . import views
-
-urlpatterns = [
-    path('car_details/<int:pk>/',views.DetailsCarView.as_view(),name='car_details'),
-]
--- carMart/order/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('create_order/<int:id>', views.buy_car, name='create_order')
-]
